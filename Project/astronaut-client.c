a69522dad81c4990a1d873604bd35b904e9338a1
Tiago Costa <tiagonarcisoudr@gmail.com>
2024-12-03 17:46:10 +0000

    Types of messages and key recognition in client side

--- Project/astronaut-client.c
+++ Project/astronaut-client.c
@@ -1,1 +1,74 @@
 #include <zmq.h>
+#include <ncurses.h>
+#include "messages.h"
+
+int main(){
+    int n = 0;
+    int key;
+    message msg; // Message to be sent to the server
+
+    // ZeroMQ TCP connection
+    void *context = zmq_ctx_new();
+    void *socket = zmq_socket(context, ZMQ_REQ);
+    zmq_connect(socket, "tcp://localhost:5555");
+
+    // Send connection message to the server
+    msg.type = ASTRONAUT_CONNECT;
+
+    // Ncurses setup
+    initscr();
+    noecho();
+    cbreak();
+    nodelay(stdscr, TRUE);
+
+    while(1){
+    	key = getch();		
+        n++;
+        switch (key){
+            // Movement keys are pressed
+            case KEY_LEFT:
+                mvprintw(0,0,"%d Left arrow is pressed", n);
+                msg.move = LEFT;
+                break;
+
+            case KEY_RIGHT:
+                mvprintw(0,0,"%d Right arrow is pressed", n);
+                msg.move = RIGHT;
+                break;
+
+            case KEY_UP:
+                mvprintw(0,0,"%d Up arrow is pressed", n);
+                msg.move = UP;
+                break;
+
+            case KEY_DOWN:
+                mvprintw(0,0,"%d Down arrow is pressed", n);
+                msg.move = DOWN;
+                break;
+
+            // Shooting key is pressed
+            case ' ':
+                mvprintw(0,0,"%d Space is pressed", n);
+                msg.type = ASTRONAUT_ZAP;
+                break;
+
+            // Exit key is pressed
+            case 'q':
+            case 'Q':
+                mvprintw(0,0,"%d q/Q is pressed", n);
+                msg.type = ASTRONAUT_DISCONNECT;
+                break;
+
+            default:
+                key = 'x'; 
+                break;
+        }
+    }
+
+    endwin(); // End ncurses
+    // ZeroMQ cleanup
+    zmq_close(socket);
+    zmq_ctx_destroy(context);
+
+    return 0;
+}
---
+++ Project/messages.h
@@ -0,0 +1,11 @@
+typedef enum move_t {UP, DOWN, LEFT, RIGHT} move_t; // Possible movements
+typedef enum msg_t {ASTRONAUT_CONNECT, ASTRONAUT_MOVEMENT, 
+                        ASTRONAUT_ZAP, ASTRONAUT_DISCONNECT,
+                        OUTER_SPACE_UPDATE} msg_t; // Types of messages
+
+typedef struct message
+{
+    msg_t type; // Type of message
+    move_t move; // Movement (for astronaut_movement messages)
+    char character; // Character of the player (for astronaut_disconnect messages)
+} message;
